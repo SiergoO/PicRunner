d1267910269528ddfe23d44ba6f8492a0443e508
sdomashchuk <Sergey.Domashchuk@softeq.com>
2022-05-21 13:26:05 +0300

    Nav component implemented.

--- app/src/main/java/com/picrunner/MainActivity.kt
+++ app/src/main/java/com/picrunner/MainActivity.kt
@@ -2,6 +2,7 @@ package com.picrunner
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
 import com.picrunner.screen.MainFragment
 
 class MainActivity : AppCompatActivity() {
@@ -9,10 +10,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commitNow()
-        }
     }
 }
